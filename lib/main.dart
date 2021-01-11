bbcfda131bc37368601116b7bcf749c035d170ba
yugeeklab <yugeeklab@gmail.com>
2021-01-11 17:55:34 +0900

    day12

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,56 @@
+import './transactioon.dart';
 import 'package:flutter/material.dart';
 
-import './quiz.dart';
-import './result.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(MyHomePage());
 }
 
 //void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Grean', 'score': 5},
-        {'text': 'White', 'score': 5}
-      ],
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Grean', 'score': 5},
-        {'text': 'White', 'score': 5}
-      ],
-    },
-    {
-      'questionText': 'What\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Grean', 'score': 5},
-        {'text': 'White', 'score': 5}
-      ],
-    },
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
-
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) print('??');
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: _questions,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, _resetQuiz),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              Column(
+                children: transactions.map((tx) {
+                  return Card(
+                    child: Text(tx.title),
+                  );
+                }).toList(),
+              ),
+            ],
+          )),
     );
   }
 }
